4b9cc43e753e75e1b96f34e5bcca998c388091b7
Jesper <jesset@student.chalmers.se>
2021-03-11 22:08:38 +0100

    Multiplayer test1

---
+++ Assets/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //connect to master server
+        PhotonNetwork.ConnectUsingSettings();
+
+        //connect to region server
+        //bool v = PhotonNetwork.ConnectToRegion("eu");
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("Connected to the " + PhotonNetwork.CloudRegion + " server!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Assets/Scripts/UI/GameSetupController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+using System.IO;
+
+public class GameSetupController : MonoBehaviour {
+
+    private Vector3 startPos = new Vector3(10, 1.5f, 12);
+    void Start() {
+        CreatePlayer();
+    }
+
+
+    private void CreatePlayer() {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), startPos, Quaternion.identity);
+    }
+}
