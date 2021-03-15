55e936c4a93351568e4af6106e3ee1be77d8b9ff
Samuel Ivarsson <isamuel@student.chalmers.se>
2021-03-15 13:41:11 +0100

    Klar med multiplayer implementation

---
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
+
+    PhotonView PV;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if (!PV.IsMine)
+        {
+            Destroy(rb);
+        }
+    }
+
+    void Update()
+    {
+        if(!PV.IsMine) return;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if(!PV.IsMine) return;
+        rb.velocity = new Vector3(horizontalInput * 4, rb.velocity.y, rb.velocity.z);
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, verticalInput * 4);
+    }
+}
---
+++ Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody rigidbodyComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumpKeyWasPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        rigidbodyComponent.velocity = new Vector3(horizontalInput * 3, rigidbodyComponent.velocity.y, rigidbodyComponent.velocity.z);
+        rigidbodyComponent.velocity = new Vector3(rigidbodyComponent.velocity.x, rigidbodyComponent.velocity.y, verticalInput * 3);
+    }
+}
