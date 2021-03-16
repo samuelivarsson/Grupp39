7cd821b599a4cb3e0a4577cb4c23fb11ae32d1c2
Samuel Ivarsson <isamuel@student.chalmers.se>
2021-03-16 11:53:18 +0100

    Ändrade namn på box till productcontroller

--- Assets/Scripts/Movables/Products/ProductController.cs
+++ Assets/Scripts/Movables/Products/ProductController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Box : MonoBehaviour
+public class ProductController : MonoBehaviour
 {
 
     //List<GameObject> droppedDeliveries = new List<GameObject>();
