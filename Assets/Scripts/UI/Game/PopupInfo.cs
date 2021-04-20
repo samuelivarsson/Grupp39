5d2fe2c23a2a498305bc5abba10cf5e9cde94651
Samuel Ivarsson <isamuel@student.chalmers.se>
2021-04-20 14:45:45 +0200

    Added possibility to show info text in game

---
+++ Assets/Scripts/UI/Game/PopupInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopupInfo : MonoBehaviour
+{
+    public static PopupInfo Instance;
+
+    [SerializeField] TMP_Text infoText;
+
+    bool timerActive = false;
+    float timer;
+
+    void Awake()
+    {
+        Instance = this;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timerActive)
+        {
+            timer -= Time.fixedDeltaTime;
+            if (timer <= 0)
+            {
+                timerActive = false;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Popup(string text, float seconds)
+    {
+        infoText.text = text;
+        timerActive = true;
+        timer = seconds;
+        gameObject.SetActive(true);
+    }
+}
